9646061ce7ec53baa877e5df0356aba945b4f153
FelipeSantos-boiBigas <felipehsantos113@gmail.com>
2026-04-15 13:33:52 -0300

    Refactor form submission event listener

--- js/orcamento.js
+++ js/orcamento.js
@@ -29,7 +29,6 @@ produtoSelect.addEventListener("change", () => {
     }
 });
 
-// envio do formulário
 document.getElementById("form-orcamento").addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -61,7 +60,7 @@ Poderia, por gentileza, me informar valores e prazos?
 
 Desde já, agradeço pela atenção.`;
 
-    const numero = "5519989874111"; // COLOQUE SEU NÚMERO
+    const numero = "5519989874111"; 
 
     window.open(`https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`);
 });
