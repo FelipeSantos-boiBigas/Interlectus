eb332e34025203c4c15e803a9c3075881f679293
FelipeSantos-boiBigas <felipehsantos113@gmail.com>
2026-04-15 10:49:51 -0300

    Increase image width in table rows

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function carregarTabela(dados) {
         const linha = document.createElement("tr");
 
         linha.innerHTML = `
-            <td><img src="${item.imagem}" width="170"></td>
+            <td><img src="${item.imagem}" width="250"></td>
             <td>${item.produto}</td>
             <td>${item.material}</td>
             <td>${item.info}</td>
