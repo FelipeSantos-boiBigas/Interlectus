e31456c894cf8bd82dc69be8bf29239e8f1cb1e3
FelipeSantos-boiBigas <felipehsantos113@gmail.com>
2026-04-15 14:58:56 -0300

    Update orcamento.js

--- js/orcamento.js
+++ js/orcamento.js
@@ -60,7 +60,7 @@ Poderia, por gentileza, me informar valores e prazos?
 
 Desde já, agradeço pela atenção.`;
 
-    const numero = "551994662703"; 
+    const numero = "5519989874111"; 
 
     window.open(`https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`);
 });
